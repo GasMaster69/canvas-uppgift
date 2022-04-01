691f7bb69e6365cae0e2d293fa893aa4d2a9fbba
GasMaster69 <philip.bergling@skola.taby.se>
2022-04-01 11:11:56 +0200

    Färdig uppgift

--- script.js
+++ script.js
@@ -21,5 +21,45 @@ Höjd på canvas: ${canvas.height}`
 
 function drawPicture() {
   // Här skriver du funktionen som ritar bilden
+  var s = document.getElementById("canvas");
+  c.beginPath();
+  c.rect(20, midY * 1.5, canvas.width / 3, midY / 2);
+  c.stroke();
+
+  c.beginPath();
+  c.rect(canvas.width - canvas.width / 3 - 20, midY * 1.5, canvas.width / 3, midY / 2);
+  c.stroke();
+
+  c.beginPath();
+  c.moveTo(20, midY * 1.5);
+  c.lineTo(canvas.width / 6 + 20, midY);
+  c.stroke();
+
+  c.beginPath();
+  c.moveTo(canvas.width / 6 + 20, midY);
+  c.lineTo(canvas.width / 3 + 20, midY * 1.5);
+  c.stroke();
+
+  c.beginPath();
+  c.moveTo(canvas.width - canvas.width / 3 - 20, midY * 1.5);
+  c.lineTo(canvas.width - canvas.width / 6 - 20, midY);
+  c.stroke();
+
+  c.beginPath();
+  c.moveTo(canvas.width - canvas.width / 6 - 20, midY);
+  c.lineTo(canvas.width - 20, midY * 1.5);
+  c.stroke();
+
+  c.beginPath();
+  c.fillStyle = 'brown';
+  c.fillRect(midX - 25, midY, 50, canvas.height / 2);
+  c.stroke();
+
+  c.beginPath();
+  c.fillStyle = 'green';
+  c.ellipse(midX, midY * 1.3, 80, 150, 0, 0.6 * Math.PI, 2.4 * Math.PI);
+  c.fill();
+  c.stroke();
 }
 drawPicture();
+
